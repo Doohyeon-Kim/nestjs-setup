50a56db1202291253ca94cbcbf89096524f1e8a5
Doohyeon Kim <92246475+Doohyeon-Kim@users.noreply.github.com>
2022-06-20 21:34:14 +0900

    Update init-nest.zsh

--- init-nest.zsh
+++ init-nest.zsh
@@ -1,8 +1,32 @@
 
-#
+#Install yarn berry
+npm install -g yarn berry
+
+# Set Yarn Berry
 yarn set version berry
+yarn -v
+yarn init
+
+# typescript
+yarn add -D typescript @tpyes/node
+yarn add -D ts-node
+
+# yarn berry recognition plug-in for vscode.
+yarn dlx @yarnpkg/sdks vscode
+
+# ZipFS for vscode to read from zip archive file.
+ZipFS -a zip file system
+
+
+yarn add rxjs
+yarn add reflect-metadata
 yarn add @nestjs/cli
 yarn add @nestjs/core
 yarn add @nestjs/common
-yarn add -D typescript @tpyes/node
+yarn add @nestjs/typeorm  
+yarn add @nestjs/swagger
+yarn add passport
+yarn add passport-jwt
+yarn add @nestjs/jwt
+yarn add @nestjs/passport
 yarn install
